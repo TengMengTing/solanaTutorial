89ac1f3fdc08381c6d15ac920e4a39a77388f00e
Mengting Teng <Mengting.teng@gmail.com>
2021-09-04 19:16:59 -0500

    updated error and created processor

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 use thiserror:: Error;
 
+use crate::{instruction::EscrowInstruction, error::EscrowError};
+
 use solana_program::program_error::ProgramError;
 
 #[derive(Error, Debug, Copy, Clone)]
